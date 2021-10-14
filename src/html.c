f7039b8832c24bfee0a7aec8b8e3a16fb2ff8e82
Christian Hau <ch.hau@gefro.de>
2021-10-14 14:39:24 +0200

    Moved HTML conversion functions to html_util.c

--- src/html.c
+++ src/html.c
@@ -13,8 +13,13 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
+/*
  * Author: christian c8121 de
+ */
+
+/*
+ * Tiny and very simple text to html converter.
+ * Suitable for output of sntools only
  */
 
 #include <stdio.h>
@@ -25,69 +30,10 @@
 #include <sys/stat.h>
 #include <time.h>
 
+#include "html_util.c"
+
 #define LINE_BUF_SIZE 512
 
-char buf[LINE_BUF_SIZE];
-char *bufp;
-
-/**
- * Print table, read more data from stdin, return if no tab found in line
- */
-void print_table() {
-
-	printf("<table border=\"0\">\n");
-	
-	do {
-
-		printf("<tr><td>");
-		
-		int n = strlen(buf);
-		for( int i=0 ; i < n ; i++ ) {
-			if( buf[i] == '\t' ) {
-				printf("</td><td>");
-			} else if( buf[i] != '\n' ) {
-				printf("%c", buf[i]);
-			}
-		}
-		
-		printf("</td></tr>\n");
-		
-		if( (bufp = fgets(buf, LINE_BUF_SIZE, stdin)) == NULL )
-			break;
-		
-	} while( index(buf, '\t') != NULL );
-	
-	
-	printf("</table>\n");
-	
-}
-
-/**
- * 
- */
-void print_paragraphs() {
-	
-	printf("<p>\n");
-	
-	do {
-		printf("%s", buf);
-		
-		if( (bufp = fgets(buf, LINE_BUF_SIZE, stdin)) == NULL )
-			break;
-		
-		if( buf[0] == '\n' ) {
-			printf("</p><p>\n");
-		} else if( index(buf, '\t') != NULL ) {
-			printf("</p>\n");
-			print_table();
-			return;
-		} else {
-			printf("<br>\n");
-		}
-	} while(1);
-	
-	printf("</p>\n");
-}
 
 /**
  * Tiny and very simple text to html converter.
@@ -96,17 +42,21 @@ void print_paragraphs() {
 int main(int argc, char *argv[]) {
 	
 	printf("<!DOCTYPE html>\n");
-	printf("<html><head><style>*{font-family: sans-serif;}td{padding:4px;border-bottom:1px solid black}</style></head><body>");
+	printf("<html><head><style>*{font-family: sans-serif;}td{padding:4px;border-bottom:1px solid black}</style></head><body>\n");
 
-	while( (bufp = fgets(buf, LINE_BUF_SIZE, stdin)) != NULL ) {
-		if( index(buf, '\t') != NULL ) {
-			print_table();
-		}
-		
-		if( bufp != NULL ) {
-			print_paragraphs();
-		}
+	char buf[LINE_BUF_SIZE];
+	while( fgets(buf, LINE_BUF_SIZE, stdin) != NULL ) {
+		html_append_text(buf);
 	}
+	html_finish();
+	
+	struct linked_item *i = html;
+	while( i != NULL ) {
+		printf("%s", (char*)i->data);
+		i = i->next;
+	}
+	
+	linked_item_free(html);
 
 	printf("</body></html>");
 }
---
+++ src/html_util.c
@@ -0,0 +1,130 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Author: christian c8121 de
+ */
+
+/*
+ * Very simple util to convert text to html
+ */
+
+#include "linked_items.c"
+
+struct linked_item *html = NULL;
+struct linked_item *html_last = NULL;
+
+#define UNDEFINED 0
+#define TEXT 1
+#define TABLE 2
+
+int html_curr_paragraph_type = UNDEFINED;
+
+
+
+
+/**
+ * 
+ */
+void _html_append(char *s, int max) {
+
+	if( html_last == NULL && html != NULL ) {
+		//Begin new conversion
+		linked_item_free(html);
+		html = NULL;
+	}
+
+	if( html == NULL ) {
+		html = malloc(sizeof(struct linked_item));
+		html_last = html;
+	} else {
+		struct linked_item *next = malloc(sizeof(struct linked_item));
+		html_last->next = next;
+		html_last = next;
+	}
+
+	if( max < 0 ) {
+		html_last->data = malloc(strlen(s)+1);
+		strcpy(html_last->data, s);
+	} else {
+		html_last->data = malloc(max+1);
+		strncpy(html_last->data, s, max);
+	}
+}
+
+/**
+ * 
+ */
+void _html_finish_block() {
+	if( html_curr_paragraph_type == TEXT) {
+		_html_append("</p>", -1);
+	}else if( html_curr_paragraph_type == TABLE) {
+		_html_append("</table>", -1);
+	}
+}
+
+
+
+/**
+ * 
+ */
+void html_append_text(char *text) {
+
+	if( strchr(text, '\t') != NULL ) {
+
+		//TABLE line
+		if( html_curr_paragraph_type != TABLE ) {
+			_html_finish_block();
+			_html_append("<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\">", -1);
+		}
+
+		html_curr_paragraph_type = TABLE;
+		_html_append("<tr><td>", -1);
+
+		char *s = text;
+		char *p = text;
+		while (p[0] != '\0') {
+			if( p[0] == '\t' ) {
+				_html_append(s, p-s);
+				_html_append("</td><td>", -1);
+				s = p+1;
+			}
+			p++;
+		}
+		_html_append(s, -1);
+
+		_html_append("</td></tr>", -1);
+	} else {
+
+		//TEXT line
+		if( html_curr_paragraph_type != TEXT ) {
+			_html_finish_block();
+			_html_append("<p>", -1);
+		}
+
+		html_curr_paragraph_type = TEXT;
+		_html_append(text, -1);
+	}
+}
+
+/**
+ * 
+ */
+void html_finish() {
+	_html_finish_block();
+	html_last = NULL;
+}
+
+
